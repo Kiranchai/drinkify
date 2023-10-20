5dc35c6efce985a5dc13afe53ab3569c52ce9af8
Kiranchai <sadawa.norbert@gmail.com>
2023-10-20 18:53:37 +0000

    x

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,8 +9,4 @@ export function middleware(request: NextRequest) {
     pathname.startsWith("/static")
   )
     return NextResponse.next();
-
-  const response: NextResponse = new NextResponse();
-  response.headers.set("Cache-Control", "public");
-  return response;
 }
